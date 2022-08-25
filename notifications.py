168be41a2f1add1119093847d4be567cbe2997e6
elizabeth <63110747+kttel@users.noreply.github.com>
2022-08-25 13:33:23 +0300

    [upd] added new features

--- notifications.py
+++ notifications.py
@@ -1,15 +1,20 @@
 from plyer import notification
+from random import choice
 
 
-class Notification():
+class Notification:
     modes = {0: "працювати", 1: "відпочивати"}
+    arr_free = ['попити води', 'зробити перекус', 'подивитись у вікно', 'пройтись по кімнаті']
+    arr_work = ['продовжити роботу', 'виконувати завдання', 'вдосконалюватись']
 
     @classmethod
     def send(cls, mode):
         action = cls.modes[mode]
+        additional_text = choice(cls.arr_free) if mode else choice(cls.arr_work)
+
         notification.notify(
             title='Pomodoro tracker',
-            message=f'Час {action}',
+            message=f'Час {action} та {additional_text}!',
             app_icon=r'icons/tomato.ico',
             timeout=10,
         )
